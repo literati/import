389db4bf0e840b1f0d7af3f3e6e42cfcd9522b4f
jason peak <jpeak5@lsu.edu>
2013-02-18 18:50:45 -0600

    removes 2 classes, adds textarea to form

--- index.php
+++ index.php
@@ -2,94 +2,8 @@
 
 //require forms lib
 require_once dirname(__FILE__) . '/lib/Nibble-Forms/Nibble/NibbleForms/NibbleForm.php';
-
-class importer {
-    
-    const FILES_DIR = 'files';
-    
-    public $data;
-    public $remote;
-    public $file_name;
-    public $local_path;
-    
-    public function __construct($url){
-        $this->remote = $url;
-        
-        $comps = preg_split("#[//]+#", $this->trim_proto($this->remote));
-        $this->file_name = array_pop($comps);
-        $this->local_path = implode('/', $comps);
-    }
-    
- public function fetch(){
-     $ch = curl_init();
-     curl_setopt($ch, CURLOPT_URL, $this->remote);
-     curl_setopt($ch, CURLOPT_HEADER, false);
-     curl_setopt($ch,  CURLOPT_RETURNTRANSFER, true);
-     $data = curl_exec($ch);
-     $this->data = htmlentities($data);
-     curl_close($ch);
-     }
-
-     private function graft_fs($url){
-         
-     }
-     
-     /**
-      * takes a URL as input and removes the protocol and the www, if present
-      * @param string $str
-      * @return string
-      */
-     private function trim_proto($str){
-         return str_ireplace(array('http://', 'https://', 'www.'), array(''), $str);
-     }
-     
-     
-     /**
-      * saves the fetched file in the local filesystem in a path resembling the original url
-      * NB: this can only be called once per file, otherwise, we will trim too much of the path
-      */
-     public function save_local(){
-         $target = self::FILES_DIR.DIRECTORY_SEPARATOR.$this->local_path;
-         
-         if(is_dir($target) or mkdir($target, 0755, true)){
-             if(($h = fopen($target.DIRECTORY_SEPARATOR.$this->file_name, 'w')) !== false){
-                 if(fwrite($h, $this->data) === false){
-                     die("could not write to file");
-                 }
-             }
-         }
-     }
-     
-     public function save_tei(){
-         
-     }
-     
-     public function getLocalPath(){
-         return self::FILES_DIR.DIRECTORY_SEPARATOR.$this->local_path.DIRECTORY_SEPARATOR.$this->file_name;
-     }
-     
-}
-
-
-class parser {
-    
-    public $source;
-    public $doc;
-    public $body;
-    public $output;
-    
-    public function __construct($infile){
-        $this->source = $infile;
-        $this->doc = new DOMDocument();
-        $this->doc->loadHTMLFile($this->source);
-        $this->body = $this->doc->getElementsByTagName('body');
-    }
-    
-    public function getParagraphs(){
-        return $this->body->item(0)->getElementsByTagName('p');
-    }
-     
-}
+require_once dirname(__FILE__).'/Parser.php';
+require_once dirname(__FILE__).'/Importer.php';
 
 
 //begin PAGE logic
@@ -103,8 +17,7 @@ if(empty($_POST['nibble_form'])){
     $f->fetch();
     $f->save_local();
 
-
-    $p = new parser($f->getLocalPath());
+    $p = new Parser($f->getLocalPath());
 
     $xml = new DOMDocument();
     $xml->load(importer::FILES_DIR.DIRECTORY_SEPARATOR.'apc-tei-bare.xml');
@@ -130,10 +43,9 @@ if(empty($_POST['nibble_form'])){
         echo "no paragraphs found...";  
     }
 
-    echo $xml->saveXML();
+    echo $xml = $xml->saveXML();
 
-
-    render_form();
+    render_form($xml);
 
 }
 
@@ -141,11 +53,16 @@ if(empty($_POST['nibble_form'])){
 
 //echo $body->p[0];
 
-function render_form(){
+function render_form($xml){
     /* Get an instance of the form called "form_one" */
     $form = \Nibble\NibbleForms\NibbleForm::getInstance('form_one');
     /* Add field using 3 arguments; field name, field type and field options */
-    $form->addField('source_url', 'url', array('required' => false));
+    $form->addField('source_url', 'url', array('required' => true));
+    $form->addField('xml', 'TextArea', 
+            array('required' => true
+                    ,'rows'  => 200
+                    ,'cols'  => 150
+                    ,'value' => $xml));
 
     echo $form->render();
 
